15c969b7540f6de83041b087fcd94e9d2387603e
dlwogns410 <dlwogns410@naver.com>
2023-11-23 02:30:09 +0900

    Menu 출력시 Login 창 제거 버전

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,34 @@
-import java.awt.Dimension;
-//import javax.swing.JFrame;
 import javax.swing.*;
+import java.awt.*;
+class Main extends JFrame {
 
-public class Main {
-   public static void main(String[] args) {
-      JPanel panel = new JPanel();
-      JFrame frame = new JFrame();
-      
-      //panel.add(new JLabel("Welcome thus lecture"));
-      frame.add(panel);
-      
-      frame.setResizable(true); //크기 고정
-      frame.setVisible(true);
-      frame.setPreferredSize(new Dimension(560,547*8/5));
-      frame.setSize(560,547*8/5); //창 크기
-      frame.setLocationRelativeTo(null);  //화면 중앙
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 다른 프레임 초기화 및 구성 요소를 여기에 추가할 수 있습니다.
-      
-   }
+    Main() {
+        this.setResizable(false);
+        this.setVisible(true);
+        
+        getContentPane().setBackground(Color.WHITE);
+        this.setBounds(0, 0, 800, (600 * 8 / 5) - 81);
+        this.setSize(1300, (600 * 8 / 5) - 81);
+
+        // Create the background panel and add it to the frame
+        BackgroundPanel backgroundPanel = new BackgroundPanel();
+        add(backgroundPanel);
+
+        
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Create and add the Login panel
+        Login login = new Login();
+        backgroundPanel.add(login);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        // Repaint the label to make sure the background image is displayed
+        backgroundPanel.repaint();
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new Main();
+        });
+    }
 }
